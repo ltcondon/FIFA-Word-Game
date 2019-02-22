99fe8db73c712fef30a3f22599d1b3399f44fc65
Liam Condon <condonliamt@gmail.com>
2019-02-21 21:37:20 -0800

    initial connections attempted b/w html and javascript variables and getElementById's

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,40 +1,57 @@
 // Possible Players: 
-var playerList = [
- player1 = "David De Gea",
- player2 = "Marcelo",
- player3 = "Sergio Ramos",
- player4 = "Raphael Varane",
- player5 = "Virgil Van Dijk",
- player6 = "N'golo Kante",
- player7 = "Luca Modric",
- player8 = "Kevin De Bruyne",
- player9 = "Kylian Mbappe",
- player10 = "Lionel Messi",
- player11 = "Christiano Ronaldo",
- player12 = "Neymar Jr",
-];
+var playerList = ["David De Gea", "Marcelo", "Sergio Ramos", "Raphael Varane", "Virgil Van Dijk", "N'golo Kante", "Luca Modric", "Kevin De Bruyne", "Kylian Mbappe", "Lionel Messi", "Christiano Ronaldo", "Neymar Jr"];
 
-// A random question is prompted:
+// Game initiated and variables assigned:
+
+//document.addEventListener (button)
+
+/// A player from the player list is randomly selected:
+var randomPlayerUpper = playerList[Math.floor(Math.random() * playerList.length)];
+var randomPlayer = randomPlayerUpper.toLowerCase();
+
+/// A series of empty spaces is generated, equal to the number of characters in the selected player's name:
+var blankPlayerGuess = [];
+    for (var i=0; i<randomPlayer.length; i++){
+    blankPlayerGuess[i] = "_";
+
+/// That series of empty spaces is displayed on the screen in the "current-player-guess" box:
+var displayGuess = document.getElementById("current-player-guess");
+    displayGuess.textContent = blankPlayerGuess;
+
+/// Initial score and remaining guesses are set:
+var score = 0;
+var remainingGuesses = 12;
+
+/// Current score and remaining guesses are displayed on screen in "score&guess-box".
+var displayScore = document.getElementById("current-score");
+    displayScore.textContent = score;
+
+var displayRemainingGuesses = document.getElementById("guesses-remaining");
+    displayRemainingGuesses.textContent = remainingGuesses;
+
+/// Incorect guesses are displayed in the "incorrect-guesses" box:
+var wrongGuesses = [];
+
+var displayWrongGuesses = document.getElementById("incorrect-guesses");
+    displayRemainingGuesses.textContent = remainingGuesses;
+
 document.onkeyup = function(event) {
+    var userGuess = event.key;
 
-if (event.keyCode == 32) {
-var currentPlayer = playerList[Math.floor(Math.random() * playerList.length)];
-var displayQuestion = document.getElementById("current-question");
-   displayQuestion.textContent = currentQuestion;
+//    if (randomPlayer.includes(userGuess)) {
+//
+//    }
+    
+    for (var j=0; j<randomPlayer.length; j++) {
+        if (randomPlayer[j] === userGuess) {
+            blankPlayerGuess[j] = userGuess;
+        }
+        else {
+            wrongGuesses.push(userGuess + ", ");
+        }
+    }
 
-// User's answer is compared to question's answer:
-if (((event.key === "t") && (a.currentQuestion === true)) || ((event.key === "f") && (a.currentQuestion === false))) {
-   var correctPrompt = document.getElementById("current-question");
-   correctPrompt.textContent = "Correct! Nice Job.";
-}
+    if (event.key {
 
-else if (((event.key === "t") && (a.currentQuestion === false)) || ((event.key === "f") && (a.currentQuestion === true))) {
-   var incorrectPrompt = document.getElementById("current-question");
-   incorrectPrompt.textContent = "Sorry! That's incorrect.";
-} 
-
-else {
-   console.log("n/a");
-}
-}
+    }
 };
