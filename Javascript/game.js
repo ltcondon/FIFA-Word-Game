b016d7979a8d652dcb87a4187e29a2913a8dd970
Liam Condon <condonliamt@gmail.com>
2019-02-22 17:04:47 -0800

    left out js file in last commit

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,79 +1,91 @@
+
 // Possible Players: 
 var playerList = ["David De Gea", "Marcelo", "Sergio Ramos", "Raphael Varane", "Virgil Van Dijk", "N'golo Kante", "Luca Modric", "Kevin De Bruyne", "Kylian Mbappe", "Lionel Messi", "Christiano Ronaldo", "Neymar Jr"];
-// Array.prototype.randsplice = function() {
-//     var randomPlayerUpper = [Math.floor(Math.random() * this.length)];
-//     var randomPlayer = this.splice(randomPlayerUpper, 1);
-//     var randomPlayer = randomPlayerUpper.toLowerCase();
-//     return randomPlayer;
-// }
+
+
 // Game initiated and variables assigned:
 
-var gameInProgress = false;
+/// Initial score and remaining guesses are set:
+var score = 0;
+var remainingGuesses = 12;
+
+/// Current score is displayed on screen in "score&guess-box".
+var displayScore = document.getElementById("current-score");
+    displayScore.textContent = score;
 
 document.getElementById("start-button").addEventListener("click", function(){
-    gameInProgress = true;
+    runGame();
     
 });
-  if (gameInProgress = true) {
       /// A player from the player list is randomly selected:
     // randsplice(playerList);
 
-    var randomPlayerUpper = playerList[Math.floor(Math.random() * playerList.length)];
-    var randomPlayer = randomPlayerUpper.toLowerCase();
-
-    /// A series of empty spaces is generated, equal to the number of characters in the selected player's name:
-    var blankPlayerGuess = [];
-        for (var i=0; i<randomPlayer.length; i++){
-        blankPlayerGuess[i] = "_";
-        }
-
-    /// That series of empty spaces is displayed on the screen in the "current-player-guess" box:
-    var displayGuess = document.getElementById("current-player-guess");
-        displayGuess.textContent = blankPlayerGuess;
-
-    /// Initial score and remaining guesses are set:
-    var score = 0;
-    var remainingGuesses = 12;
-
-    /// Current score and remaining guesses are displayed on screen in "score&guess-box".
-    var displayScore = document.getElementById("current-score");
-        displayScore.textContent = score;
-
-    var displayRemainingGuesses = document.getElementById("guesses-remaining");
-        displayRemainingGuesses.textContent = remainingGuesses;
-
-    /// Incorect guesses are displayed in the "incorrect-guesses" box:
-    var wrongGuesses = [];
-    var displayWrongGuesses = document.getElementById("incorrect-guesses");
-        displayWrongGuesses.textContent = wrongGuesses;
       
-    document.onkeyup = function(event) {
-          
-        var userGuess = event.key;
-        if (remainingGuesses > 0) {
-            for (var j=0; j<randomPlayer.length; j++) {
-                if (randomPlayer[j] === userGuess) {
-                    blankPlayerGuess[j] = userGuess;
-                    displayGuess.textContent = blankPlayerGuess;
-                 }
-              }
-                    
-            if (!randomPlayer.includes(userGuess)){
-                  remainingGuesses--;
-                  wrongGuesses.push(userGuess + ", ");
-                  displayRemainingGuesses.textContent = remainingGuesses;
-                  displayWrongGuesses.textContent = wrongGuesses;
-              }
-            
-            if (!blankPlayerGuess.includes("_")) {
-                      alert("You won!");
-                      return;
-              }
-            }
-        }
-        else {
-            alert("You lost!");
-        }
-          
+    function runGame() {
 
+
+        var randomPlayerUpper = playerList[Math.floor(Math.random() * playerList.length)];
+        var randomPlayer = randomPlayerUpper.toLowerCase();
+    
+        /// A series of empty spaces is generated, equal to the number of characters in the selected player's name:
+        var blankPlayerGuess = [];
+            for (var i=0; i<randomPlayer.length; i++){
+            blankPlayerGuess[i] = "_";
+            }
+    
+        /// That series of empty spaces is displayed on the screen in the "current-player-guess" box:
+        var displayGuess = document.getElementById("current-player-guess");
+            displayGuess.textContent = blankPlayerGuess;
+    
+        /// Initial remaining guesses are set:
+        var remainingGuesses = 12;
+    
+    
+        var displayRemainingGuesses = document.getElementById("guesses-remaining");
+            displayRemainingGuesses.textContent = remainingGuesses;
+    
+        /// Incorect guesses are displayed in the "incorrect-guesses" box:
+        var wrongGuesses = [];
+        var displayWrongGuesses = document.getElementById("incorrect-guesses");
+            displayWrongGuesses.textContent = wrongGuesses;
+
+        document.onkeyup = function(event) {
+            var allowedGuesses = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+            var userGuessUpper = event.key;
+            var userGuess = userGuessUpper.toLowerCase(); 
+            
+            function compare() {
+
+                for (var j=0; j<randomPlayer.length; j++) {
+                    if (randomPlayer[j] === userGuess) {
+                        blankPlayerGuess[j] = userGuess;
+                        displayGuess.textContent = blankPlayerGuess;
+                     }
+                  }
+                        
+                if(!randomPlayer.includes(userGuess)){
+                      remainingGuesses--;
+                      wrongGuesses.push(userGuess + ", ");
+                      displayRemainingGuesses.textContent = remainingGuesses;
+                      displayWrongGuesses.textContent = wrongGuesses;
+                  }
+            }
+            if (allowedGuesses.includes(userGuess)) {
+                compare()  
+             }
+             
+              
+        
+          if(!blankPlayerGuess.includes("_")) {
+            alert("You won!");
+            score++;
+            displayScore.textContent = score;
+            runGame();
+          }
+
+          if (remainingGuesses < 1) {
+              alert("You lost!");
+              runGame();
+          }
+        }
     }
