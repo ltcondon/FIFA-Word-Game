ad6446a4949764dfb19f4aa1a996768569711bf9
Liam Condon <condonliamt@gmail.com>
2019-02-22 22:21:38 -0800

    organized and restructured js file, and added comments throughout to better summarize what each line of code is doing

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,19 +1,41 @@
 
 // Possible Players: 
-var playerList = ["David De Gea", "Marcelo", "Sergio Ramos", "Raphael Varane", "Virgil Van Dijk", "N'golo Kante", "Luca Modric", "Kevin De Bruyne", "Kylian Mbappe", "Lionel Messi", "Christiano Ronaldo", "Neymar Jr"];
+var playerList = ["David De Gea", "Marcelo", "Sergio Ramos", "Raphael Varane", "Virgil Van Dijk", "N'golo Kante", "Luka Modric", "Kevin De Bruyne", "Kylian Mbappe", "Lionel Messi", "Christiano Ronaldo", "Neymar Jr"];
 
 
 // Game initiated and variables assigned:
 
 /// Initial score and remaining guesses are set:
 var score = 0;
-var remainingGuesses = 12;
+var remainingGuesses = 10;
 
 /// Current score is displayed on screen in "score&guess-box".
 var displayScore = document.getElementById("current-score");
     displayScore.textContent = score;
 
+// Game restarts if user reaches end of array or runs out of guesses:
+function restart() {
+    
+    // Resets options in array:
+    playerList = ["David De Gea", "Marcelo", "Sergio Ramos", "Raphael Varane", "Virgil Van Dijk", "N'golo Kante", "Luka Modric", "Kevin De Bruyne", "Kylian Mbappe", "Lionel Messi", "Christiano Ronaldo", "Neymar Jr"];    
+    
+    // Resets any progress from previous attempt(s):
+    score = 0;
+    remainingGuesses = 10;
+    wrongGuesses = [];
+    
+    // Displays correct stats on-screen:
+    displayScore.textContent = score;    
+    displayWrongGuesses = document.getElementById("incorrect-guesses");
+            displayWrongGuesses.textContent = wrongGuesses;
+
+    var displayEmpty = document.getElementById("current-player-guess");
+            displayEmpty.textContent = "WHO AM I??";
+}
+
+// Start button starts/resets game: 
 document.getElementById("start-button").addEventListener("click", function(){
+    restart();
     runGame();
     
 });
@@ -23,7 +45,9 @@ document.getElementById("start-button").addEventListener("click", function(){
     function runGame() {
 
       /// A player from the player list is randomly selected:
-        var randomPlayerUpper = playerList[Math.floor(Math.random() * playerList.length)];
+        var randomPlayerIndex = [Math.floor(Math.random() * playerList.length)];
+        var randomPlayerUpper = playerList[randomPlayerIndex];
+        var removeSelection = playerList.splice(randomPlayerIndex, 1);
         var randomPlayer = randomPlayerUpper.toLowerCase();
     
         /// A series of empty spaces is generated, equal to the number of characters in the selected player's name:
@@ -38,17 +62,14 @@ document.getElementById("start-button").addEventListener("click", function(){
                 else if (randomPlayer[k] === "'") {
                     blankPlayerGuess[k] = "'";
                 }
-            blankPlayerGuess
             }
     
         /// That series of empty spaces is displayed on the screen in the "current-player-guess" box:
         var displayGuess = document.getElementById("current-player-guess");
             displayGuess.textContent = blankPlayerGuess;
     
-        /// Initial remaining guesses are set:
-        var remainingGuesses = 12;
-    
-    
+        /// Initial remaining guesses are set and displayed:
+        var remainingGuesses = 10;
         var displayRemainingGuesses = document.getElementById("guesses-remaining");
             displayRemainingGuesses.textContent = remainingGuesses;
     
@@ -57,10 +78,14 @@ document.getElementById("start-button").addEventListener("click", function(){
         var displayWrongGuesses = document.getElementById("incorrect-guesses");
             displayWrongGuesses.textContent = wrongGuesses;
 
+        /// Once a guess is made (key is pressed), guess is first verified as a possible character, then compared to the random player:
         document.onkeyup = function(event) {
             var allowedGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
             var userGuessUpper = event.key;
             var userGuess = userGuessUpper.toLowerCase(); 
+            if (allowedGuesses.includes(userGuess)) {
+                compare();
+             }
             
             function compare() {
 
@@ -70,7 +95,8 @@ document.getElementById("start-button").addEventListener("click", function(){
                         displayGuess.textContent = blankPlayerGuess;
                      }
                   }
-                        
+                
+                //// If letter has not been previously guessed, it is pushed to the screen either into the current guessword (if correct), or into the incorrect guesses' box:
                 if ((!randomPlayer.includes(userGuess)) && (!wrongGuesses.includes(" " + userGuess))) {
                       remainingGuesses--;
                       wrongGuesses.push(" " + userGuess);
@@ -78,12 +104,10 @@ document.getElementById("start-button").addEventListener("click", function(){
                       displayWrongGuesses.textContent = wrongGuesses;
                   }
             }
-            if (allowedGuesses.includes(userGuess)) {
-                compare()  
-             }
+
              
               
-        
+          /// Once current guessword is completely filled with correct characters, the user is alerted they were correct, their score increases, and a new guessword is generated:
           if(!blankPlayerGuess.includes("_")) {
             alert("You won!");
             score++;
@@ -91,9 +115,10 @@ document.getElementById("start-button").addEventListener("click", function(){
             runGame();
           }
 
+          /// If the user runs out of guesses, they are alerted they've lost the game, and the game resets:
           if (remainingGuesses < 1) {
               alert("You lost!");
-              runGame();
+              restart();
           }
         }
     }
